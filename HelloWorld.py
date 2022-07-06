341f7282da4084d0aa2b87458f39b52cb99a4478
APLLE <aplle@APLLEs-MacBook-Pro.local>
2022-07-06 16:53:27 +0100

    first commit

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+print ("Hello World")
+population = "House"
+print(population)
+population =100
+print (population)
---
+++ Variables.py
@@ -0,0 +1,7 @@
+ourVariable = 14.0
+ourVariableType = type(ourVariable)
+print(ourVariableType)
+ourNewVariable = int(ourVariable)
+print(ourNewVariable)
+ourNewVariableType = type(ourNewVariable)
+print (ourNewVariableType)
